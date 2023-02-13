d724bfb8f5bfa45727f59fba1a63f3a57d356f3d
mirjam <mirjam.schliephack@haw-hamburg.de>
2023-02-13 16:06:05 +0100

    mover ordering and result graph

---
+++ negamax_ab_move_ordering.py
@@ -0,0 +1,47 @@
+from math import inf as infinity
+
+
+class NegamaxAlphaBetaMoveOrdering:
+    version = '_v3'
+
+    def __init__(self):
+        self.positionCount = 0
+        self.explorationOrder = [3, 2, 4, 5, 1, 0, 6]
+
+    def getScore(self, board):
+        return int((board.rows * board.columns - board.numberOfPiecesPlayed + 1) / 2)
+
+    def _negamax(self, board, alpha, beta):
+        self.positionCount += 1
+        if board.isFull():
+            return 0
+
+        for i in range(board.columns):
+            if not board.columnIsFull(i) and board.wouldBeWin(i):
+                return self.getScore(board)
+
+        max_score = int((board.columns * board.rows - 1 - board.numberOfPiecesPlayed) / 2)
+        if beta > max_score:
+            beta = max_score
+            if alpha >= beta:
+                return beta
+
+        for i in self.explorationOrder:
+            if not board.columnIsFull(i):
+                column_index, row_index = board.insertPiece(i)
+
+                score = -self._negamax(board, -beta, -alpha)
+
+                board.fields[row_index][column_index] = None
+                board.numberOfPiecesPlayed = board.numberOfPiecesPlayed - 1
+
+                if score >= beta:
+                    return score
+
+                alpha = max(alpha, score)
+
+        return alpha
+
+    def solve(self, board):
+        self.positionCount = 0
+        return self._negamax(board, -infinity, infinity)
--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,9 @@
 from minimax import Minimax
 from negamax import Negamax
 from negamax_alpha_beta import NegamaxAlphaBeta
+from negamax_ab_move_ordering import NegamaxAlphaBetaMoveOrdering
 
 # solver = Minimax()
 # solver = Negamax()
-solver = NegamaxAlphaBeta()
+# solver = NegamaxAlphaBeta()
+solver = NegamaxAlphaBetaMoveOrdering()
